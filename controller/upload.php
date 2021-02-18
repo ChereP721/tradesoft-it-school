82112ecb341688c165552ae7ebc325e6aca3890f
Anton510 <anton.shubin.1989@mail.ru>
2021-02-18 20:59:05 +0300

    lesson upload file

--- controller/upload.php
+++ controller/upload.php
@@ -2,14 +2,7 @@
 
 // загрузка картинки
 
-var_dump($_FILES);
-
-if (isset($_FILES) && !empty($_FILES)) {
-    $target = "images/image1.jpg";
-    move_uploaded_file($_FILES["image"]["tmp_name"], $target);
-}
-
-
+move_uploaded_file($_FILES['image']['tmp_name'], '../uploads/' . uniqid() . '.jpg');
 
 
 ?>
