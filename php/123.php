0d360c8dfeececb6fdd053d092b6b535ddf455f5
avshatov <Per-ferus@mail.ru>
2021-03-09 23:12:00 +0300

    создание 2-х форм (наброски)

--- php/123.php
+++ php/123.php
@@ -21,10 +21,55 @@
                 <input type="checkbox" id="hideButton" onclick="changeVisibility('buttonBlock', checked)"/>Hide Button
             </tr>
         </form>
+        
         <script type="text/javascript" language="JavaScript">
             function changeVisibility(divId, visible){
                 document.getElementById(divId).style.display = visible ? "none": "block";
             }
         </script>
+
+
+<!-- моя форма -->
+<div class="auth__form shadow">  
+<form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>"  enctype="multipart/form-data">
+        <input type="hidden" name="form-name" value="form-auth" >
+            <div class="auth__group">
+                <label class="auth__label auth_login" for="authLogin">Логин</label>
+                <input class="auth__input" type="text" id="authLogin" name="authLogin" placeholder="Введите логин">
+            </div>
+            <div class="auth__group auth_password">
+                <label class="auth__label" for="password">Пароль</label>
+                <input class="auth__input" type="password" id="password1" name="password1" placeholder="Введите пароль">
+            </div>
+            <button class="btn" id="" type="submit" name="authSubmit">Войти</button>                        
+            <button class="btn" id="" type="submit" name="authregSubmit">Зарегистрироваться</button>
+</form>
+</div> 
+<div class="reg__form shadow">  
+<form method="POST" action="<?php echo $_SERVER['PHP_SELF'];?>" enctype="multipart/form-data">
+        <input type="hidden" name="form-name" value="form-reg" >
+            <div class="reg__group">
+                <label class="reg__label reg_login" for="regLogin">Логин</label>
+                <input class="reg__input" type="text" id="regLogin" name="regLogin" placeholder="Введите логин">
+            </div>
+            <div class="reg__group reg_password1">
+                <label class="reg__label" for="password1">Введите пароль</label>
+                <input class="reg__input" type="password" id="password1" name="password1" placeholder="Введите пароль">
+            </div>
+            <div class="reg__group reg_password2">
+                <label class="reg__label" for="password2">Повторите пароль</label>
+                <input class="reg__input" type="password" id="password2" name="password2" placeholder="Повторите пароль">
+            </div>                  
+            <button class="btn" id="" type="submit" name="Back">Назад</button>
+            <button class="btn" id="" type="submit" name="regSubmit">Зарегистрироваться</button>
+</form>
+</div>
+
+
+
+
+
+<link rel="stylesheet" href="../css/MyStyle.css" type = "text/css">
+
     </body>
 </html>
